61c7bc645cb159dc7c0c56a83d5e1bd5767fd481
Jeffrey Lin <jeffrey@icurse.nl>
2018-01-24 21:17:21 -0500

    Remove the concept of server/client mode
    The text editor should always run in client mode, and enable server mode
    when requested.

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include ".h"
 
 const char *argp_program_bug_address = "anaveragehuman AT icurse DOT nl";
-const char doc[] = "a text editor with the ability to edit over networks"
-                   "\vClients must specify the port number to connect to.\n"
-                   "Servers must specify the name of the file to edit.";
-static char args_doc[] = "file";
-struct arguments arguments = {0, "0.0.0.0", 0, 0};
+const char doc[] = "a text editor with the ability to edit over networks";
+static char args_doc[] = "FILE";
+struct arguments arguments = {"0.0.0.0", "0"};
 
 static struct argp_option options[] = {
-  {"server", 's', 0     , 0, "enable server functionality"}        ,
-  {"ip"    , 'i', "IP"  , 0, "the IP to bind or connect to"}       ,
-  {"port"  , 'p', "port", 0, "the port to bind or connect to"}     ,
-  {"file"  , 'f', "file", 0, "the file to edit (server mode only)"},
+  {"ip"   , 'i', "IP"  , 0, "the IP to bind or connect to"}   ,
+  {"port" , 'p', "port", 0, "the port to bind or connect to"} ,
   {0}
 };
 
@@ -19,16 +15,16 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   struct arguments *arguments = state->input;
 
   switch (key) {
-    case 's':
-      arguments->servermode = 1;
-      break;
     case 'i':
       arguments->ip = arg;
       break;
     case 'p':
       arguments->port = arg;
       break;
-    case 'f':
+    case ARGP_KEY_ARG:
+      if (state->arg_num > 0) {
+        argp_error(state, "expected at most one filename");
+      }
       arguments->filename = arg;
       break;
     default:
@@ -44,22 +40,7 @@ char loop = 1;
 
 int main( int argc, char *argv[] ){
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
-
-  if (! (arguments.servermode || arguments.port) ) {
-    die("Clients must supply at least the port number.", 1);
-  }
-
-  if (arguments.servermode && ! arguments.filename) {
-    die("Server must specify the file to edit.", 1);
-  } // XXX: clients ignore the filename; user should be aware of this
-
-
-  if (arguments.servermode) {
-    server();
-  } else {
-    client();
-  }
-
+  client();
   return 0;
 }
 
